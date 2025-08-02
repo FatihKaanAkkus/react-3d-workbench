e1e38c76b5048a19416e24a423b3d60d910c5382
FatihKaanAkkus <fthknakkus@hotmail.com>
2025-08-02 17:40:34 +0300

    Add packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import { Box, CameraControls, OrbitControls, View } from "@react-three/drei";
 import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { useRef } from "react";
 
 function App() {
-  return <></>;
+  const container = useRef<HTMLElement>(null!);
+
+  return (
+    <main ref={container}>
+      <View style={{ width: "50%" }}>
+        <Box>
+          <meshNormalMaterial />
+        </Box>
+        <OrbitControls />
+      </View>
+      <View style={{ width: "50%" }}>
+        <Box>
+          <meshNormalMaterial />
+        </Box>
+        <CameraControls />
+      </View>
+      <Canvas
+        eventSource={container}
+        eventPrefix="client"
+        id="canvas"
+        style={{ position: "fixed", inset: 0 }}
+      >
+        <View.Port />
+      </Canvas>
+    </main>
+  );
 }
 
 export default App;
